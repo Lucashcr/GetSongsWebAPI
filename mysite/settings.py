3b762ef68cad280fe15355cbf88a23c170f60640
Lucashcr <lucash.rocha@hotmail.com>
2023-02-17 19:01:15 -0300

    Uninstall allauth app

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,10 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
     
     'rest_framework',
 
