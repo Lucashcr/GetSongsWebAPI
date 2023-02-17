878125d226545ab54698bdeb60e2d641a335a467
Lucashcr <lucash.rocha@hotmail.com>
2023-02-17 18:27:27 -0300

    Set configurations

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    
+    'rest_framework',
+
+    'core',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -112,9 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
