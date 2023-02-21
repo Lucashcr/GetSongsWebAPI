d7849e6704ca3db0330098f7f781e2507b10cb27
Lucashcr <lucash.rocha@hotmail.com>
2023-02-21 01:13:12 -0300

    Set verbose name to models' fields

--- core/models.py
+++ core/models.py
@@ -3,15 +3,41 @@ from django.contrib.auth.models import User
 
 from api.models import Song
 
-# Create your models here.
+
 class Hymnary(models.Model):
-    title = models.CharField(max_length=64)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    print_category = models.BooleanField(default=True)
-    songs = models.ManyToManyField(Song, related_name='hymns', through='HymnarySong')
+    title = models.CharField('Título', max_length=64)
+    owner = models.ForeignKey(
+        User,
+        verbose_name='Proprietário',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    print_category = models.BooleanField('Imprimir categoria', default=True)
+    songs = models.ManyToManyField(
+        Song,
+        verbose_name='Músicas',
+        related_name='hymns',
+        through='HymnarySong'
+    )
+
 
 class HymnarySong(models.Model):
-    song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='song_hymnaries')
-    hymnary = models.ForeignKey(Hymnary, on_delete=models.CASCADE, related_name='hymnary_songs')
-    order = models.IntegerField(unique=True)
+    song = models.ForeignKey(
+        Song,
+        verbose_name='Música',
+        on_delete=models.CASCADE,
+        related_name='song_hymnaries'
+    )
+    hymnary = models.ForeignKey(
+        Hymnary,
+        verbose_name='Hinário',
+        on_delete=models.CASCADE,
+        related_name='hymnary_songs'
+    )
+    order = models.IntegerField('Ordem', unique=True)
+
+
+# class Contact(models.Model):
+#     name = models.CharField(max_length=64)
+#     email = models.EmailField(max_length=128)
+#     message = models.TextField()
