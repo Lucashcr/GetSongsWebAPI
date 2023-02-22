af3c784fa66d423df8cf9e684a468d8ee1c1fd0f
Lucashcr <lucash.rocha@hotmail.com>
2023-02-22 12:38:48 -0300

    Adjust api show songs view 3

--- api/views.py
+++ api/views.py
@@ -54,8 +54,8 @@ class ShowSongsView(TemplateView):
         context_data = super().get_context_data(**kwargs)
         context_data['collection'] = Song.objects.all()
 
-        slug = kwargs.pop('slug', None)
-        if slug is not None:
+        slug = kwargs.pop('slug', '')
+        if slug != '':
             artists = Song.objects.filter(artist__slug=slug)
             categories = Song.objects.filter(categories__slug=slug)
 
