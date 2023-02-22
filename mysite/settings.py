37710cd12c803f159e4d3ef2ceb4fe899e689852
Lucashcr <lucash.rocha@hotmail.com>
2023-02-22 10:43:57 -0300

    Use enviroment to set email settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,8 +146,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'getsongsweb@gmail.com'
-EMAIL_HOST_PASSWORD = 'gugogmwwvffuyiqo'
+EMAIL_HOST = os.environ["EMAIL_HOST"]
+EMAIL_PORT = os.environ["EMAIL_PORT"]
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_USE_TLS = True
