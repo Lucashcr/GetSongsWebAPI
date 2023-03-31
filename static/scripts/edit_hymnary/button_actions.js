ecbb118bfe0b628ffd81e9f0dca74b6f99ecd220
Lucashcr <lucash.rocha@hotmail.com>
2023-03-31 00:34:40 -0300

    Improves interface to edit_hymnary page

--- static/scripts/edit_hymnary/button_actions.js
+++ static/scripts/edit_hymnary/button_actions.js
@@ -2,35 +2,55 @@ function togglePopUp() {
     document.querySelector(".pop-up").toggleAttribute("hidden");
 }
 
-function appendSong() {
-    songs.then(data => {
-        let song = data.find(song => song.id == song_select.value);
+async function appendSong() {
+    const song = await songs.find(song => song.id == song_select.value);
+    const artist = await artists.find(artist => artist.id == song.artist);
+    const category = await categories.find(category => category.id == song.category);
 
-        let newSongElement = document.createElement("tr");
-        newSongElement.classList.add("song_item");
-        newSongElement.draggable = true;
+    let newSongElement = document.createElement("div");
+    newSongElement.classList.add("song_item border border-secondary");
+    newSongElement.draggable = true;
 
-        let category = document.createElement("td");
-        categories.then(data => {
-            category.innerText = data.find(cat => song.category == cat.id).name;
-            newSongElement.appendChild(category);
-            let songname = document.createElement("td");
-            songname.innerText = song.name;
-            newSongElement.appendChild(songname);
-            let artist = document.createElement("td");
-            artists.then(data => {
-                artist.innerText = data.find(art => song.artist == art.id).name;
-                newSongElement.appendChild(artist);
-                let song_id_input = document.createElement("input");
-                song_id_input.type = "hidden";
-                song_id_input.classList.add("song_id");
-                song_id_input.value = song.id;
-                newSongElement.appendChild(song_id_input)
+    newSongElement.innerHTML = `<div>
+            <h3 class="name">${song.name}</h3>
+            <h4 class="artist">${song.artist}</h4>
+        </div>
+        <h4 class="category">${song.category}</h4>
+        <input type="hidden" class=${song.id} value="{{item.song.id}}">`;
+    // songs.then(data => {
+    //     let song = data.find(song => song.id == song_select.value);
 
-                songs_list.insertAdjacentElement("beforeend", newSongElement);
-            })
-        })
-    })
+    //     let newSongElement = document.createElement("div");
+    //     newSongElement.classList.add("song_item border border-secondary");
+    //     newSongElement.draggable = true;
+
+    //     newSongElement.innerHTML = `<div>
+    //             <h3 class="name">${song.name}</h3>
+    //             <h4 class="artist">${song.artist}</h4>
+    //         </div>
+    //         <h4 class="category">${song.category}</h4>
+    //         <input type="hidden" class=${song.id} value="{{item.song.id}}">`;
+    //     let category = document.createElement("td");
+    //     categories.then(data => {
+    //         category.innerText = data.find(cat => song.category == cat.id).name;
+    //         newSongElement.appendChild(category);
+    //         let songname = document.createElement("td");
+    //         songname.innerText = song.name;
+    //         newSongElement.appendChild(songname);
+    //         let artist = document.createElement("td");
+    //         artists.then(data => {
+    //             artist.innerText = data.find(art => song.artist == art.id).name;
+    //             newSongElement.appendChild(artist);
+    //             let song_id_input = document.createElement("input");
+    //             song_id_input.type = "hidden";
+    //             song_id_input.classList.add("song_id");
+    //             song_id_input.value = song.id;
+    //             newSongElement.appendChild(song_id_input)
+
+    //             songs_list.insertAdjacentElement("beforeend", newSongElement);
+    //         })
+    //     })
+    // })
 }
 
 function removeSong() {
