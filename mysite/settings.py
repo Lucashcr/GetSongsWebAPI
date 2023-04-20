160948f0c7a8db5f55a68522d236a0130b04cc16
Lucashcr <lucash.rocha@hotmail.com>
2023-04-20 14:07:36 -0300

    Remove ALLOW_ORIGINS=[*] from settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,8 +33,6 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get('DEPLOY', None) else True
 
-ALLOWED_HOSTS = ["*"]
-
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
 CSRF_TRUSTED_ORIGINS = ['https://getsongs.up.railway.app']
