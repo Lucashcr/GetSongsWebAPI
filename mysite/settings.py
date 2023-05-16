ea0383a436045a046b2f22b062931338d05b8613
Lucashcr <lucash.rocha@hotmail.com>
2023-05-16 13:21:56 -0300

    Use environment variables settings 4

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = bool(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = ['getsongs.up.railway.app']
 
