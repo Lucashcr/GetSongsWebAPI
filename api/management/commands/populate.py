744392fe92f7d5eb09053460a59c333717292bb5
Lucashcr <lucash.rocha@hotmail.com>
2024-02-02 17:05:32 -0300

    Implementa comando de populate

---
+++ api/management/commands/populate.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from api.models import Song
+
+
+class Command(BaseCommand):
+    help = "Closes the specified poll for voting"
+
+    def add_arguments(self, parser):
+        parser.add_argument("index")
+
+    def handle(self, *args, **options):
+        if options["index"] == "songs":
+            indexed, added, from_db = Song.populate()
+            self.stdout.write(f'Indexed: {indexed} | New: {added} | New from database: {from_db}', self.style.SUCCESS)
+        else:
+            raise CommandError("Invalid index")
