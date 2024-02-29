c1bc562cea84914c3f3e6437bedb6e05570cd948
Lucas Rocha <lucash.rocha@hotmail.com>
2024-02-29 02:19:47 -0300

    Implements background on pdf report

--- build_doc/templates.py
+++ build_doc/templates.py
@@ -4,6 +4,14 @@ from reportlab.platypus import *
 from .styles import *
 
 
+def page_setup(canvas, doc):
+    canvas.saveState()
+    canvas.drawImage(
+        "build_doc/images/background.png", x=0, y=0, width=210 * mm, height=297 * mm
+    )
+    canvas.restoreState()
+
+
 class CustomBaseTemplate(BaseDocTemplate):
     def __init__(self, filename, **kw):
         super().__init__(filename, **kw)
@@ -13,12 +21,10 @@ class CustomBaseTemplate(BaseDocTemplate):
         self.__body.append(Paragraph(text, style))
 
     def insert_paragraph_link(self, text, href, style=LEFT_ALIGNED):
-        self.__body.append(
-            Paragraph(f'<a href="{href}">{text}</a>', style))
+        self.__body.append(Paragraph(f'<a href="{href}">{text}</a>', style))
 
     def insert_heading_link(self, text, href, style=HEADING_1):
-        self.__body.append(
-            Paragraph(f'<a href="{href}">{text}</a>', style))
+        self.__body.append(Paragraph(f'<a href="{href}">{text}</a>', style))
 
     def insert_heading(self, text, style=HEADING_1):
         self.__body.append(Paragraph(text, style))
@@ -36,10 +42,9 @@ class SingleColumnTemplate(CustomBaseTemplate):
         super().__init__(filename, **kw)
 
         page = Frame(
-            self.leftMargin, self.bottomMargin,
-            self.width, self.height, id='page'
+            self.leftMargin, self.bottomMargin, self.width, self.height, id="page"
         )
-        self.addPageTemplates(PageTemplate('single_column', [page]))
+        self.addPageTemplates(PageTemplate("single_column", [page], onPage=page_setup))
 
 
 class TwoColumnsTemplate(CustomBaseTemplate):
@@ -48,14 +53,22 @@ class TwoColumnsTemplate(CustomBaseTemplate):
         super().__init__(filename, **kw)
 
         col1 = Frame(
-            self.leftMargin, self.bottomMargin,
-            self.width/2 - 5*mm, self.height, id='col1'
+            self.leftMargin,
+            self.bottomMargin,
+            self.width / 2 - 5 * mm,
+            self.height,
+            id="col1",
         )
         col2 = Frame(
-            self.width/2 + self.leftMargin + 5*mm, self.bottomMargin,
-            self.width/2 - 5*mm, self.height, id='col2'
+            self.width / 2 + self.leftMargin + 5 * mm,
+            self.bottomMargin,
+            self.width / 2 - 5 * mm,
+            self.height,
+            id="col2",
         )
-        self.addPageTemplates(PageTemplate('two_columns', [col1, col2]))
+        self.addPageTemplates(
+            PageTemplate("two_columns", [col1, col2], onPage=page_setup)
+        )
 
 
-__all__ = ['SingleColumnTemplate', 'TwoColumnTemplate']
+__all__ = ["SingleColumnTemplate", "TwoColumnTemplate"]
